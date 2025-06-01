73c78c723284ff5feb02f915905628412ab61e90
Ankit Kumar <ankitkr5447@gmail.com>
2025-06-01 19:41:44 +0530

    first commit

---
+++ src/main/java/com/feedback_project/feedbackApp/App.java
@@ -0,0 +1,13 @@
+package com.feedback_project.feedbackApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
